473b436eb1081725dd91f2fd1852e46ddc7664a3
mhmmd-iqbal <mhmmd.iqbal.june@gmail.com>
2022-08-20 14:40:57 +0700

    update

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,17 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function apiRespond(string $message = '', $data = [], int $http_code = 200) {
+        if( $http_code !== 200 ) {
+            return response()->json([
+                'message'   => $message,
+            ], $http_code);
+        }
+        
+        return response()->json([
+            'message'   => $message,
+            'result'    => $data,
+        ], $http_code);
+    }
 }
